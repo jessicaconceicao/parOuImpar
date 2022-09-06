b46c2b73304c83c3dd6bdf7203acfacca2923eaf
jessicaconceicao <jessicacampos.dev@gmail.com>
2022-09-05 21:37:34 -0300

    Adicionar arquivos de projeto.

---
+++ parOuImpar/Program.cs
@@ -0,0 +1,19 @@
+﻿//Verificar se a entrada é par ou ímpar
+Console.WriteLine("Digite um número");
+string numero = Console.ReadLine();
+try
+{
+    int numeroInt = Convert.ToInt32(numero);
+    if (numeroInt % 2 == 0)
+    {
+        Console.WriteLine("O número é par.");
+    }
+    else
+    {
+        Console.WriteLine("O número é impar.");
+    }
+}
+catch (Exception)
+{ 
+    Console.WriteLine("Digite um número válido"); 
+}
